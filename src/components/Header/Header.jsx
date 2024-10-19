502530f94c4ce1379b81ee1d1d0e1386d0384334
Bia <agiampedro@gmail.com>
2024-10-19 18:43:30 -0300

    style: Adiciona estilo ao header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import style from './header.module.css'
 
 function Header() {
   return (
-    <header>
+    <header className={style.header}>
+      <div className={style.logo}>
+        <img src="./imgs/logo.svg" alt="" />
+        <span>Movie Apps</span>
+      </div>
+        
         <nav>
             <a href="/">Início</a>
             <a href="/create">Criar</a>
