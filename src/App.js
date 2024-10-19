d1ee01801832a05c2cbb2673cff0f58c8494f8cf
Bia <agiampedro@gmail.com>
2024-10-19 17:45:43 -0300

    feat: Cria rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Read from './Pages/Read/Read';
+import Remove from './Pages/Remove/Remove';
+import Update from './Pages/Update/Update';
+import Create from './Pages/Create/Create';
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Header/>
+
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/create' element={<Create/>}/>
+        <Route path='/read/:id' element={<Read/>}/>
+        <Route path='/remove' element={<Remove/>}/>
+        <Route path='/update' element={<Update/>}/>
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
