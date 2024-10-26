e48ee54f69131e0fac621122bc2b0ea421902313
Bia <agiampedro@gmail.com>
2024-10-26 15:05:39 -0300

    feat: Adiciona componente alert

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import style from './alert.module.css'
 
 function Error({message}) {
   return (
-    <div>
+    <div className={style.alert}>
         <p>{message}</p><br/>
-        <Link to="/">Voltar</Link>
+        <Link className={style.link} to="/">Voltar</Link>
     </div>
   )
 }
