6263df18d758288b623c6e17fc28c94573e46a68
Bia <agiampedro@gmail.com>
2024-10-26 15:06:10 -0300

    style: Estiliza o header

--- src/Pages/Create/Create.jsx
+++ src/Pages/Create/Create.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Link, useNavigate} from 'react-router-dom'
 import axios from 'axios';
+import style from './create.module.css'
 
 function Create() {
 
@@ -21,19 +22,29 @@ function Create() {
   }
 
   return (
-    <div>
+    <div className={style.divForm}>
+      <h1>Cadastrar Filme</h1>
       <form action="">
-        <label htmlFor="">Nome:</label><br />
-        <input type="text"  onChange={(e)=>setValues({...values, name:e.target.value})} /><br />
+        <div>
+          <label htmlFor="">Nome:</label><br />
+          <input type="text"  onChange={(e)=>setValues({...values, name:e.target.value})} /><br />
+        </div>
 
-        <label htmlFor="">Gênero:</label><br />
-        <input type="text" onChange={(e)=>setValues({...values, genre:e.target.value})}/><br />
-
-        <label htmlFor="">Ano:</label><br />
-        <input type="text" onChange={(e)=>setValues({...values, any:e.target.value})}/><br />
-
-        <button onClick={createMovie}>Criar</button>
-        <Link to="/">Cancelar</Link>
+        <div>
+          <label htmlFor="">Gênero:</label><br />
+          <input type="text" onChange={(e)=>setValues({...values, genre:e.target.value})}/><br />
+        </div>
+        
+        <div>
+          <label htmlFor="">Ano:</label><br />
+          <input type="text" onChange={(e)=>setValues({...values, any:e.target.value})}/><br />
+        </div>
+        
+        <div className={style.events}>
+          <button onClick={createMovie}>Criar</button>
+          <Link className={style.link} to="/">Cancelar</Link>
+        </div>
+        
       </form>
     </div>
   )
